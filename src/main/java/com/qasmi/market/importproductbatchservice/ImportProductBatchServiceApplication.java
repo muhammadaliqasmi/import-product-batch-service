b9a3f614cd71f3a27f70627b1c951a756b553c5c
Muhammad Ali Qasmi <aqasmi@etilizepak.com>
2020-07-09 14:17:10 +0500

    - Adds project's skeleton
    - Adds contributing

---
+++ src/main/java/com/qasmi/market/importproductbatchservice/ImportProductBatchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.qasmi.market.importproductbatchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportProductBatchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportProductBatchServiceApplication.class, args);
+	}
+
+}
